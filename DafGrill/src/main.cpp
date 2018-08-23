7ef5106615805c16e75b4752001e9faa6d616c3d
Daniel Kierkegaard Andersen <dani485b@gmail.com>
2018-08-23 13:37:26 +0200

    Some basic structure

--- DafGrill/src/main.cpp
+++ DafGrill/src/main.cpp
@@ -1,9 +1,14 @@
 #include <Arduino.h>
+#include <Stepper.h>
+
+Stepper MotorSpear = Stepper(200, 9,10,11,12);
 
 void setup() {
     // put your setup code here, to run once:
+    MotorSpear.setSpeed(0);
 }
 
 void loop() {
     // put your main code here, to run repeatedly:
+    MotorSpear.step(60);
 }
