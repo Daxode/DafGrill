50da23f0601a65ffd16d6b93699c6feedda4f504
Daniel Kierkegaard Andersen <dani485b@gmail.com>
2018-08-28 10:40:13 +0200

    Spear motor stuff

--- DafGrill/src/Header Files/SpearMotor.h
+++ DafGrill/src/Header Files/SpearMotor.h
@@ -1,2 +1,5 @@
 //This is the control for the spear motor it should take an input rotation speed
 //and be able to control it constantly at that speed until you decide to turn it off.
+
+//It should include a method for rotating the motor
+ void Rotate();
